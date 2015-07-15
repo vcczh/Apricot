a0ae66702f87f1e53f2dcc9197857f6ce1597053
yular <1062969706@qq.com>
2015-07-14 19:06:52 -0700

    routers js update

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,9 @@
 var express = require('express');
-
-var db = require('../models/dbhandler');
-
 var router = express.Router();
 
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next){
+    res.render('index');
 });
 
-/* Test Db connection with sample query */
-router.get('/testDB', function (req, res) {
-	db.test(req, res);
-})
+module.export = router;
 
-module.exports = router;
